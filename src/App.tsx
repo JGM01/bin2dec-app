5e69f99794cc9bfc43ee3fdd8d92f457103f6e1e
JGM01 <jacobgermanamccray@gmail.com>
2023-02-22 21:36:12 -0500

    prepared

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,59 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  // Create a "State Hook"
+  // Sets an object with default attributes, and creates a setter function.
+  const [binary, setBinary] = useState({
+    val: '',
+    isBinary: false,
+  });
+
+  // Same thing here, but I don't need default atributes.
+  const [dec, setDec] = useState(0);
+
+  useEffect(() => {
+    if (binary.val.length >= 0 && binary.isBinary) {
+      // Set the decimal val to the bin -> dec conversion.
+      setDec(4);
+    } 
+  }, [binary]);
+
+  // Create regular expression to determine if valid binary.
+  const isInValidBinary: RegExp = /[^01]/;
+
+  function handleBinaryInput(event: React.FormEvent<HTMLInputElement>): void {
+    const value = event.currentTarget.value;
+
+    setBinary({
+      val: value,
+      isBinary: !isInValidBinary.test(value),
+    });
+  };
+
+  function binToDec(bin: string): number {
+    let result = 0;
+
+    bin
+      .split('')
+      .reverse()
+      .map((val, idx) => {
+        return val === '1' && (result += Math.pow(2, idx));
+      });
+    return result;
+  } 
+
+  
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Convert Binary to Decimal
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
